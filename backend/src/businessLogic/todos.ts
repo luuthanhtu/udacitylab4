4838fd6a52d0af5cf86cdd98bf77a37db19836b8
TEST <TEST@example.com>
2022-05-18 13:11:09 +0700

    Update

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -3,7 +3,7 @@ import 'source-map-support/register'
 import * as uuid from 'uuid'
 
 import { TodosAccess } from './TodosAccess'
-import { connectToS3 } from './attachmentUtils'
+import { connectToS3 } from '../dataConnect/attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { TodoUpdate } from '../models/TodoUpdate'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
