3d16fdbae0010a1f0e23dc461babf0b1ae90cdb4
unknown <luuthanhtu2296@gmail.com>
2022-06-02 08:24:07 +0700

    Update

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '53tdik6cn1'
+const apiId = 'dj2eebx0c7'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
