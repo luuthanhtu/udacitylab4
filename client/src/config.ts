af17a96fb2beb2bc14d82ed1ffea5839f3d8d3c6
TEST <TEST@example.com>
2022-06-08 06:03:18 +0700

    Fix delete Todo

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'u3vawjtly9'
+const apiId = '53tdik6cn1'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
